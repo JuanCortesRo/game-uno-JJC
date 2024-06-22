23bc9c39c79a1fe609bb3d88f86213dabe672e74
Juan José Cortés Rodríguez <juan.jose.cortes@correounivalle.edu.co>
2024-06-22 01:13:55 -0500

    Commit 11.01: Last Commit

--- src/main/java/org/example/eiscuno/model/game/GameUno.java
+++ src/main/java/org/example/eiscuno/model/game/GameUno.java
@@ -38,7 +38,7 @@ public class GameUno implements IGameUno {
     @Override
     public void startGame() {
         for (int i = 0; i < 10; i++) {
-            if (i < 5) {
+            if (i < 4) {
                 humanPlayer.addCard(this.deck.takeCard());
             } else {
                 machinePlayer.addCard(this.deck.takeCard());
